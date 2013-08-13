d0cd5d4ebc1171187a681281a70d5a8fde2d1fb5
NIkola Mitrovic <nikkolica@gmail.com>
2013-08-13 16:56:50 +0200

    Added Lesson 1 for Data Structures

---
+++ Lesson1/Array.cpp
@@ -0,0 +1,60 @@
+// EXAMPLE OF METHODS DEFINED IN Array.h
+
+#include "Array.h"
+#include <iostream.h>
+
+
+void main()
+{
+	Array<int> obj(5,5);
+	try
+	{ // inserting elements into array
+		obj.insert(3,9);
+		obj.insert(2,8);
+		obj.insert(1,7);
+		obj.insert(0,6);
+		obj.insert(-1,5);
+	
+    // creating another object and trying out functions	
+	Array<int> obj1(10,5);
+	obj1=obj;
+	obj.print();
+	
+	cout<<obj[5]<<endl;
+	cout<<obj[6]<<endl;
+	cout<<obj[7]<<endl;
+	cout<<obj[8]<<endl;
+	obj1.print(); // printing array
+	obj1.bubble(); // bubble sort
+	obj1.print(); // printing array
+	cout<<obj.getBase()<<endl;
+	//obj.setBase(10);
+	cout<<obj.getBase()<<endl;
+	cout<<"niz je velicine "<<obj.getLength()<<endl;
+	
+	Array<int> obj2(6); // trying for the second array
+	obj2.insert(3,0);
+	obj2.insert(7,1);
+	obj2.insert(3,5);
+	obj2.insert(4,3);
+	obj2.insert(2,2);
+	
+	// trying out function for the first derivate
+	cout<<"polinom: ";
+	obj2.print();
+	int stepen=2;
+	Array<int> obj3(6);
+	obj3.izvod(obj2,stepen);
+	cout<<stepen<<". izvod:";
+	obj3.print();
+	/*Array<int> obj4(6);
+	obj4=obj2.izvod(1);
+	obj4.print();*/
+	}
+	catch(char* e)
+	{
+		cout<<e<<endl;
+	}
+	
+}
+
---
+++ Lesson1/Array.h
@@ -0,0 +1,146 @@
+template <class T>
+class Array
+{
+private:
+	int length; // array length
+	int base; // base
+	T* data;// array pointer
+public:
+	Array() // default constructor
+	{
+		length=0;
+		base=0;
+		data=new T[length];
+		for(int i=0;i<length;i++)
+			data[i]=0; 
+	}
+	Array(int len) // initaly base is set to 0
+	{
+		length=len;
+		base=0;
+		data=new T[length];
+		for(int i=0;i<length;i++)
+			data[i]=0;
+	}
+	Array(int len, int b) // whan base is set to value b
+	{
+		length=len;
+		base=b;
+		data=new T[length];
+		for(int i=0;i<length;i++)
+			data[i]=0;
+	}
+	~Array() // destructor
+	{
+		delete[] data;
+	}
+	void insert(T el,int poz); // for inserting elements into array
+	void print(); // for printing array
+	int getBase() 
+	{
+		return base;
+	}
+	void setBase(int b)
+	{
+		base=b;
+	}
+	int getLength()
+	{
+		return length;
+	}
+	void setLength(int newLen);
+	Array& operator=(Array& a);
+	T operator[](int index);
+	void bubble();
+	//Array<T> izvod(int stepen);
+	void izvod(Array<T>& a,int stepen);
+};
+
+// IMPLEMENTATION OF METHODS
+template <class T>
+void Array<T>::print()   // printing array
+{
+	for(int i=0;i<length;i++)
+		cout<<data[i]<<" ";
+	cout<<endl;
+}
+ template <class T>
+ void Array<T>::insert(T el, int poz) // inserting element to certain position
+ {
+	 int offset=poz-base;
+	 if(length<=offset)
+		 throw "index je van granica";
+	 else
+		data[offset]=el;
+ }
+template <class T>
+void Array<T>::setLength(int newLen) // setting the length of the array
+{
+	if(length!=newLen)
+	{
+		T* newData=new T[newLen];
+		int min=length;
+		if(min>newLen)
+			min=newLen;
+		for(int i=0;i<min;i++)
+			newData[i]=data[i];
+		delete[] data;
+		data=newData;
+		
+	}
+}
+template <class T>
+Array<T>& Array<T>::operator =(Array<T>& a)  // operator =
+{
+	base=a.base;
+	if(length!=a.length)
+	{
+		length=a.length;
+		delete[] data;
+		data=new T[length];
+	}
+	for(int i=0;i<length;i++)
+		data[i]=a.data[i];
+	return *this;
+}
+template <class T>
+T Array<T>::operator [](int index) // operator []
+{
+	int offset=index-base;
+	if  (length<=offset)
+		throw "index je van opsega";
+	return data[offset];
+}
+template <class T>
+void Array<T>::bubble()  // bubble sort
+{
+	int tmp;
+	for(int i=0;i<length-1;i++)
+	{
+		for(int j=0;j<length-i-1;j++)
+		{
+			if(data[j]<data[j+1])
+			{
+				tmp=data[j];
+				data[j]=data[j+1];
+				data[j+1]=tmp;
+			}
+		}
+	}
+
+}
+template <class T>
+void Array<T>::izvod(Array<T>& a,int stepen)  //  first derivate of polynoim where 'a[i]' is coeficiente and 'i' is 'degree'
+{
+	for(int j=0;j<stepen;j++)
+	{
+		int len=length-1;
+		for(int i=len;i>0;i--)
+		{
+			data[i-1]=a.data[i]*i;
+			a.data[i]=data[i];
+		}
+		a.data[0]=0;
+	}
+		
+}
