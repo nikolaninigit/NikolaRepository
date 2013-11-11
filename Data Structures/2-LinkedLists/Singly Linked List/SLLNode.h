1878eac96e8f20813532cb6a7d3f267f694ce974
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 01:48:07 +0100

    Update SLLNode.h

--- Data Structures/2-LinkedLists/Singly Linked List/SLLNode.h
+++ Data Structures/2-LinkedLists/Singly Linked List/SLLNode.h
@@ -3,41 +3,40 @@ template<class t>
 class SLLNode
 {
    public:
-    t info; // info -> generaly Node value
-	SLLNode<t> *next; // pointer to next node
+       	t info; // info -> generaly Node value
+   	SLLNode<t> *next; // pointer to next node
 
    public: // Methods
 	   // default constructor
-	   SLLNode() { next = NULL; }
+	SLLNode() { next = NULL; }
+          
+   	SLLNode( t i ) // constructor with arguments
+   	{
+   	   info = i;
+	   next = NULL;
+   	}
 
-	   SLLNode( t i ) // constructor with arguments
-	   {
-           info = i;
-		   next = NULL;
-	   }
+   	// constructor with arguments
+   	SLLNode(t i , SLLNode<t> * n)
+   	{
+	    info = i;
+	    next = n;
+   	}
 
-	   // constructor with arguments
-	   SLLNode(t i , SLLNode<t> * n)
-	   {
-		    info = i;
-			next = n;
-	   }
+   	// destructor
+   	~SLLNode()
+   	{
+	}
 
-	   // destructor
-	   ~SLLNode()
-	   {
+   	// method for getting info value
+   	t print()
+   	{
+  	 return info;
+   	}
 
-	   }
-
-	   // method for getting info value
-	   t print()
-	   {
-          return info;
-	   }
-
-       // method for checking if argument is equal to info 'value'
-	   bool isEqual(t el)
-	   {
-           return (info == el);
-	   }
+	// method for checking if argument is equal to info 'value'
+   	bool isEqual(t el)
+   	{
+   	return (info == el);
+   	}
 };
