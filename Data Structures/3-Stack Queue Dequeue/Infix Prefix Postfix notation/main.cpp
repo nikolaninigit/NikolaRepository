775f06f053a12c9f3e1f653cc46ec4959f93f428
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 18:41:50 +0100

    Update main.cpp

--- Data Structures/3-Stack Queue Dequeue/Infix Prefix Postfix notation/main.cpp
+++ Data Structures/3-Stack Queue Dequeue/Infix Prefix Postfix notation/main.cpp
@@ -5,68 +5,67 @@ void main()
 {
 	try
 	{
-      StackAsArray<char *>  obj(50); 
-      
-	 // example 1 
-     //char *e ="( ( ( a*b + c*d)^f + (d+e)^m ) / g )";
-     // char *e ="(a+(b+c+d)*e)";
-
-     // example 2	
-	 //  char *e="ab+c*d/ef^+g/";
-     
-    	    
-	 // example - from prefix to infix | set template to char *
-	   char *e="*a/+bcd";
-	   obj.prefixInfix(e);  
-	   obj.print(); 
-       cout<<endl;
-	   
-/*
-	// example - from prefix to infix | set template to char *
-	     char *e="ab+c*d/"; 
-	     obj.postfixInfix(e);
-         obj.print();
+		StackAsArray<char *>  obj(50); 
+		
+		// example 1 
+		//char *e ="( ( ( a*b + c*d)^f + (d+e)^m ) / g )";
+		// char *e ="(a+(b+c+d)*e)";
+		
+		// example 2	
+		//  char *e="ab+c*d/ef^+g/";
+		
+		
+		// example - from prefix to infix | set template to char *
+		char *e="*a/+bcd";
+		obj.prefixInfix(e);  
+		obj.print(); 
+		cout<<endl;
+		
+		/*
+		// example - from prefix to infix | set template to char *
+		char *e="ab+c*d/"; 
+		obj.postfixInfix(e);
+		obj.print();
 		 cout<<endl;
-*/
-
-	// example 3
-	//	char *e="*7/+543";
-	//	char *e="+43";
-
-	  
-	 /* 
+		*/
+		
+		// example 3
+		//	char *e="*7/+543";
+		//	char *e="+43";
+		
+		
+		/* 
 		//example - from prefix to infix | set template to char *	
 		char *e="( ( (a+b)*c/d + e^f) /g ) ";
 		obj.infixPostfix(e);
 		obj.print(); // stampanje magacina za prevodjenje izraza kad koristim //
-
-	 */
-
-
-	/*  
+		
+		*/
+		
+		
+		/*  
 		  // example - from prefix to infix | set template to char *	
 		  char *e="( ( (a+b)*c/d + e^f) /g ) ";
 		  obj.infixPrefix(e);
 		  obj.print(); // stampanje magacina za prevodjenje izraza kad koristim //
-	*/
-
-
-      
-	/*
-	    //example - calculating value in postfix notation | set template to double
+		*/
+		
+		
+		
+		/*
+		//example - calculating value in postfix notation | set template to double
 		 char *e="*7/+543";
 		 cout<<obj.racunajPrefix(e);
 		 
 		 cout<<endl;
-	 */
-
-
-	/*
-	   //example - calculating value in postfix notation | set template to double 
-	   char *e = "72+4*3/";
-	   cout<<obj.postfix07(e)<<endl;
-	 */    
-
+		*/
+		
+		
+		/*
+		//example - calculating value in postfix notation | set template to double 
+		char *e = "72+4*3/";
+		cout<<obj.postfix07(e)<<endl;
+		*/    
 	}
 	catch(char *izuz)
 	{
