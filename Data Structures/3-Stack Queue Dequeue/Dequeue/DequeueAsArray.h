8d89b827fd71425244b2793391b6eb2852394ed1
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 18:20:51 +0100

    Update DequeueAsArray.h

--- Data Structures/3-Stack Queue Dequeue/Dequeue/DequeueAsArray.h
+++ Data Structures/3-Stack Queue Dequeue/Dequeue/DequeueAsArray.h
@@ -9,90 +9,86 @@ class DequeueAsArray
 		int numOfElements; // number of elements in the deqeuue
 		int head; // begining of the dequeue
 		int tail; // ending of the dequeue
-
-    DequeueAsArray( int n) // constructor 
+	
+	DequeueAsArray( int n) // constructor 
 	{
 		size = new t[n];
 		numOfElements = 0;
-        head = -1;
+		head = -1;
 		tail = -1;
-
+	
 	}
-
+	
 	~DequeueAsArray() // destructor
 	{
 		delete [] niz;
 		numOfElements = 0;
-        head = -1;
+		head = -1;
 		tail = -1;
 	}
-   
+	
 	bool isEmpty(); // checks if dequeue is empty    
 	void enqueueTail(t elem); // function for adding elements to the "end-tail" of the Dequeue
-    t dequeueTail(); // function for taking elements from the "end-tail" of the Dequeue
+	t dequeueTail(); // function for taking elements from the "end-tail" of the Dequeue
 	void enqueueHead(t elem); // function for adding elements to the "begining-head" of the Dequeue
 	t dequeueHead(); // function for taking elements from the "end-tail" of the Dequeue
 };
-
-template<class t> // checks if dequeue is empty
-bool DequeueAsArray<t>::isEmpty()
-{
-  return ( numOfElements ==0 )
-}
-
-template<class t> // function for adding elements to the "end-tail" of the Dequeue
-void DequeueAsArray<t>::enqueueTail(e elem)
-{
-   if ( numOfElements == size )
-	    throw "NE MOZE enqueueTail() - DEK JE PUN ";
-   
-   tail = tail+1;
-   if ( tail == size )
-	   tail ==0;
-
-   niz[tail] = elem;
-
-   if( numOfElements ==0 )
-	     head = tail;
-    
-    numOfElements= numOfElements+1;
-}
-
-template<class t> // function for taking elements from the "end-tail" of the Dequeue
-t DequeueAsArray<t>::dequeueTail()
-{
-   if( numOfElements == 0 )
-	   throw "IZUZETAK dequeueTail() - NEMA NISTA, RED JE PRAZAN";
-
-    t elem = niz[tail];
-    
-	tail = tail -1;
-	if(tail <0)
-		tail = size -1;
-     numOfElements--;
-
-
-	return elem;
-}
-
-template<class t> // function for adding elements to the "begining-head" of the Dequeue
-void DequeueAsArray<t>::enqueueHead(t elem)
-{
-    if( numOfElements == size )
-		throw "IZUZETAK enqueueHead() - PRAZAN DEK";
-
-	if( numOfElements ==0)
-	{ 
-		head = 0;
-		tail = 0;
-	}
-	else
+	template<class t> // checks if dequeue is empty
+	bool DequeueAsArray<t>::isEmpty()
 	{
-       head = head-1;
-	   if(head <0)
-		   head = size -1;
+	  return ( numOfElements ==0 )
 	}
-	niz[head] = elem;
-	numOfElements = numOfElements + 1;
-}
-
+	
+	template<class t> // function for adding elements to the "end-tail" of the Dequeue
+	void DequeueAsArray<t>::enqueueTail(e elem)
+	{
+	   if ( numOfElements == size )
+		    throw "EXCEPTION-DEQUEUE IS FULL ";
+	   
+	   tail = tail+1;
+	   if ( tail == size )
+		   tail ==0;
+	
+	   niz[tail] = elem;
+	
+	   if( numOfElements ==0 )
+		     head = tail;
+	    
+	    numOfElements= numOfElements+1;
+	}
+	
+	template<class t> // function for taking elements from the "end-tail" of the Dequeue
+	t DequeueAsArray<t>::dequeueTail()
+	{
+	   if( numOfElements == 0 )
+		   throw "EXCEPTION-DEQUEUE IS FULL";
+	
+	    	t elem = niz[tail];
+	    
+		tail = tail -1;
+		if(tail <0)
+			tail = size -1;
+	     	numOfElements--;
+		return elem;
+	}
+	
+	template<class t> // function for adding elements to the "begining-head" of the Dequeue
+	void DequeueAsArray<t>::enqueueHead(t elem)
+	{
+	    if( numOfElements == size )
+			throw "EXCEPTION-DEQUEUE IS EMPTY";
+	
+		if( numOfElements ==0)
+		{ 
+			head = 0;
+			tail = 0;
+		}
+		else
+		{
+	       	  head = head-1;
+		   if(head <0)
+			   head = size -1;
+		}
+		niz[head] = elem;
+		numOfElements = numOfElements + 1;
+	}
