b61ae578f59db9fb2b8d84e8c1324572ac485681
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 17:18:59 +0100

    Update DLList.h

--- Data Structures/2-LinkedLists/Doubly Linked List/DLList.h
+++ Data Structures/2-LinkedLists/Doubly Linked List/DLList.h
@@ -40,113 +40,113 @@ class DLList
 
 };
 
-//functions that prints element from the list
-template<class t>
-void DLList<t>::printAll()
-{
-   DLLNode<t> *p = head;
-
-   while( p != NULL )
-   {
-	   cout<<p->print()<<"   ";
-	   p = p->next;
-
-   }
-}
-
-//  function for adding elements to the begining of the list
-template<class t>
-void DLList<t>::addToHead(t el)
-{
-	// if list is empty
-	if( head == NULL )
+	//functions that prints element from the list
+	template<class t>
+	void DLList<t>::printAll()
 	{
-		head = new DLLNode<t>(el);
-		if ( tail == NULL )
-			tail = head;
+	   DLLNode<t> *p = head;
+	
+	   while( p != NULL )
+	   {
+		   cout<<p->print()<<"   ";
+		   p = p->next;
+	
+	   }
 	}
-	else  // if list is not empty
+	
+	//  function for adding elements to the begining of the list
+	template<class t>
+	void DLList<t>::addToHead(t el)
 	{
-       head = new DLLNode<t>(el , NULL , head);
-	   head->next->prev = head;
+		// if list is empty
+		if( head == NULL )
+		{
+			head = new DLLNode<t>(el);
+			if ( tail == NULL )
+				tail = head;
+		}
+		else  // if list is not empty
+		{
+	       head = new DLLNode<t>(el , NULL , head);
+		   head->next->prev = head;
+		}
 	}
-}
-
-// function for deleting elements from the begining
-template<class t>
-t DLList<t>::deleteFromHead()
-{
-
-    // I supose that list is not empty
-	t el = head->info;
-
-	DLLNode<t> *tmp = head;
-
-	// if list has only 1 element
-	if( head == tail )
+	
+	// function for deleting elements from the begining
+	template<class t>
+	t DLList<t>::deleteFromHead()
 	{
-       head = NULL;
-	   tail = NULL;
+	
+	    // I supose that list is not empty
+		t el = head->info;
+	
+		DLLNode<t> *tmp = head;
+	
+		// if list has only 1 element
+		if( head == tail )
+		{
+	       head = NULL;
+		   tail = NULL;
+		}
+	   else
+	   { // if list has more that 1 element
+		   
+		   head = head->next;
+		   head->prev = NULL;
+	   }
+	
+	    delete tmp; 
+		return el;
+	
+	
 	}
-   else
-   { // if list has more that 1 element
-	   
-	   head = head->next;
-	   head->prev = NULL;
-   }
-
-    delete tmp; 
-	return el;
-
-
-}
-// destructor
-template<class t>
-DLList<t>::~DLList()
-{
-   while ( head != NULL )
-	   t tmp = deleteFromHead();
-}
-
-// function for deleting elements from the end of the list
-template<class t>
-t DLList<t>::deleteFromTail()
-{
-  t el = tail->info;
-  DLLNode<t> *tmp = tail;
-
-  // if list has only 1 element
-  if ( head == tail )
-  {
-     head = NULL;
-	 tail = NULL;
-  }
-  else
-  //  if list has more than 1 element
-  tail = tail->prev;
-  tail->next = NULL;
-
-  delete tmp;
-
-  return el;
-}
-
-// Function for adding elements to the end of the list
-template<class t>
-void DLList<t>::addToTail( t el )
-{
-   // is list is empty
-	if( head == NULL )
+	// destructor
+	template<class t>
+	DLList<t>::~DLList()
 	{
-       head = new DLLNode<t>(el);
-
-	   if ( tail == NULL )
-		   tail = head;
+	   while ( head != NULL )
+		   t tmp = deleteFromHead();
 	}
-	else
-	{ // if list has at least one element
-		tail->next = new DLLNode<t>(el , NULL , NULL );
-		tail->next->prev = tail;
-		tail = tail->next;
+	
+	// function for deleting elements from the end of the list
+	template<class t>
+	t DLList<t>::deleteFromTail()
+	{
+	  t el = tail->info;
+	  DLLNode<t> *tmp = tail;
+	
+	  // if list has only 1 element
+	  if ( head == tail )
+	  {
+	     head = NULL;
+		 tail = NULL;
+	  }
+	  else
+	  //  if list has more than 1 element
+	  tail = tail->prev;
+	  tail->next = NULL;
+	
+	  delete tmp;
+	
+	  return el;
 	}
-}
+	
+	// Function for adding elements to the end of the list
+	template<class t>
+	void DLList<t>::addToTail( t el )
+	{
+	   // is list is empty
+		if( head == NULL )
+		{
+	       head = new DLLNode<t>(el);
+	
+		   if ( tail == NULL )
+			   tail = head;
+		}
+		else
+		{ // if list has at least one element
+			tail->next = new DLLNode<t>(el , NULL , NULL );
+			tail->next->prev = tail;
+			tail = tail->next;
+		}
+	}
