7969161fc11c1c8a82433f5379b641ca32f46b9d
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 18:07:34 +0100

    Update main.cpp

--- Data Structures/3-Stack Queue Dequeue/Stack/main.cpp
+++ Data Structures/3-Stack Queue Dequeue/Stack/main.cpp
@@ -10,30 +10,25 @@ void main()
    try
    {
     
-	   StackAsArray<int> obj(10);
-	   StackAsArray<int> obj1(10);
-	   
-	   obj.push(1);
-	   obj.push(2);
-	   obj.push(3);
-	   obj.push(4);
-       obj.push(5);
-	   obj.push(6);
-	   obj.push(7);
+	StackAsArray<int> obj(10);
+	StackAsArray<int> obj1(10);
+	
+	obj.push(1);
+	obj.push(2);
+	obj.push(3);
+	obj.push(4);
+	obj.push(5);
+	obj.push(6);
+	obj.push(7);
+	obj.print();
+	cout<<endl;
 
-	    obj.print();
-		cout<<endl;
-
-///////////////////////
-
-  obj.reverse();
-  obj.print();
-   }
-   catch( char *e )
-   {
-       cout<<e<<endl;
-   }
-
-   
+	obj.reverse();
+	obj.print();
+	}
+	catch( char *e )
+	{
+	  cout<<e<<endl;
+	}
 }
 
