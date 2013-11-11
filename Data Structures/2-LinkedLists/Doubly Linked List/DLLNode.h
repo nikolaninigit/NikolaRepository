3e37de9edf54c115a9ed85988559639d3ebb4dee
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 17:17:57 +0100

    Update DLLNode.h

--- Data Structures/2-LinkedLists/Doubly Linked List/DLLNode.h
+++ Data Structures/2-LinkedLists/Doubly Linked List/DLLNode.h
@@ -3,46 +3,45 @@ template<class t>
 class DLLNode
 {
 	public:
-    t info;
-	DLLNode<t> *prev, *next;
+         	t info;
+     		DLLNode<t> *prev, *next;
 
 	// default constructor
 	DLLNode()
 	{
-		prev = NULL;
-        next = NULL;
+	    prev = NULL;
+	    next = NULL;
 	}
-
+	
 	// constructor with arguments-only value
 	DLLNode( t el )
 	{
-       info = el;
+	   info = el;
 	   next = NULL;
 	   prev = NULL;
 	}
 	// constructor with parameters- "value" and
 	DLLNode(t el , DLLNode<t> *p , DLLNode<t> *s )
 	{
-       info = el;
+	   info = el;
 	   prev = p ;
 	   next = s;
 	}
-
+	
 	// destructor
 	~DLLNode()
 	{
-
 	}
-
+	
 	//for ptinting
 	t print()
 	{
-		return info;
+	   return info;
 	}
-
+	
 	// if node value is equal to some value
 	bool isEqual( t el )
 	{
-       return ( el == info );
+	   return ( el == info );
 	}
 };
