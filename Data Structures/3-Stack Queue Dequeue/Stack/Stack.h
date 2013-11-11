c3c1c8fcafb2cdc039cf5b0f8ebfb8381eadfedc
NIkola Mitrovic <nikolaninigit@users.noreply.github.com>
2013-11-11 17:48:57 +0100

    Update Stack.h

--- Data Structures/3-Stack Queue Dequeue/Stack/Stack.h
+++ Data Structures/3-Stack Queue Dequeue/Stack/Stack.h
@@ -7,19 +7,19 @@ class Stack
 	
 	virtual t getTop()    // virtual function for getting from the top
 	{
-		throw "exception - call virtuAl function";
+	throw "exception - call virtuAl function";
 	};
-
-    virtual void push( t elem ) // virtual function for adding elements 
+	
+	virtual void push( t elem ) // virtual function for adding elements 
 	{
-      throw "exception-virtual function call";
+	throw "exception-virtual function call";
 	};
-
-    virtual t pop() // virtual function for getting elements
+	
+	virtual t pop() // virtual function for getting elements
 	{
-		throw "Exception-virtual function call";
+	throw "Exception-virtual function call";
 	}; 
-
+	
 	virtual bool isEmpty() { return true; }; // checking if stack is empty
 	virtual int numOfElements() {return 0 ;} ; // returning number of elements from the stack
 };
